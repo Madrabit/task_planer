016c440f5f4903eaa070213337bd3d623210534c
madrabit <madrabot@gmail.com>
2021-03-13 11:36:21 +0300

    a lot of smth

--- task-planer/src/app/app.module.ts
+++ task-planer/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { TaskListComponent } from './task-list/task-list.component';
+import { StatisticComponent } from './task-list/statistic/statistic.component';
+import { ListRowComponent } from './task-list/list-row/list-row.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TaskListComponent,
+    StatisticComponent,
+    ListRowComponent
   ],
   imports: [
     BrowserModule,
